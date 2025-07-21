ebd3568ad167d9346b339ee444602bb5e194ebae
Abhay-989 <abhaychouhan8799@gmail.com>
2025-07-22 00:08:48 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,37 +2,42 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-import {connectDB} from './config/db.js';
+import { connectDB } from './config/db.js';
 import routes from './routes/route.js';   
 import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
-if(process.env.NODE_ENV !== 'production') {
-  app.use(cors({
-  origin: "http://localhost:5173", 
-}));
-}
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: "http://localhost:5173", 
+  }));
+}
 
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use("/api/notes",routes);
+app.use("/api/notes", routes);
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(dirname,"../frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend","dist","indexhtml"));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-connectDB().then(()=>{
-app.listen(PORT, () => {
-  console.log('Server is running on port', PORT);
-});
-})
 
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+  });
+});
