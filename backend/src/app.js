48ecf7d3bb937bd3d67d441216e185207a09011d
Abhay-989 <abhaychouhan8799@gmail.com>
2025-07-21 23:54:00 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import {connectDB} from './config/db.js';
 import routes from './routes/route.js';   
@@ -10,10 +11,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors({
+const __dirname = path.resolve();
+if(process.env.NODE_ENV !== 'production') {
+  app.use(cors({
   origin: "http://localhost:5173", 
 }));
+}
 
 
 app.use(express.json());
@@ -21,6 +24,12 @@ app.use(rateLimiter);
 
 app.use("/api/notes",routes);
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(dirname,"../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend","dist","indexhtml"));
+});
+}
 connectDB().then(()=>{
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -1,4 +1,7 @@
 import axios from 'axios';
+//in production therre's no localhost so we have to make this dynamic
+const BASE_UEL = import.meta.env.Mode === "development" ? "htttp://localhost:3000/api" : "/api";
+
 const api=axios.create({
     baseURL: 'http://localhost:3000/api',
 });
